b0cde1453762e605894ec2e7fa28712d714365f1
Allison Harden <harden.allison@gmail.com>
2021-08-18 20:12:59 -0600

    Front end set up, not connected to backend.

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+import React, { useState, Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+  } from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">Minute Taker</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar >
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/outOfThePark"></NavLink>
+                                    GitHub
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavbar;
