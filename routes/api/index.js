4734870240609a5de6cba2fe7c77553cc08d1a80
Allison Harden <harden.allison@gmail.com>
2021-08-28 21:11:09 -0600

    More file cleanup now that api is graphql.

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const meetingRoutes = require('./meeting-route');
-const userRoutes = require('./user-route');
-
-router.use('/meetings', meetingRoutes);
-router.use('/users', userRoutes);
-
-module.exports = router;
